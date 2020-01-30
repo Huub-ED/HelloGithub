3d8c49969b018902a4bb4df442cd5ab1bdb694fc
Huub-ED <60462480+Huub-ED@users.noreply.github.com>
2020-01-30 16:18:56 +0800

    这是根据《Thinking in java》写的小练习

---
+++ HelloDate.java
@@ -0,0 +1,21 @@
+package java_think;
+
+//:object/HelloDate.java
+import java.util.*;
+
+//import static net.mindview.util.Print.*;
+//¼ò»¯´úÂë    System.out.print£¨£©¡ª¡ª¡ª¡ª>print()
+public class HelloDate {
+	/**Entry point to class & application.
+	 * 
+	 * @param args array of string arguments
+	 * @throws exceptions No exceptions thrown
+	 */
+	public static void main (String[] args) {
+		System.out.print("Hello,it's:");
+		System.out.print(new Date());
+	}
+}/* Output:(55% match)
+Hello,it's:
+Wed Oct 05 14:39:36 MDT 2005
+*///
